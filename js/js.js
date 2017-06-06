1c6cafff7d70861a1ce90f52a1df1d8287bf29b1
dvikster <dvikster@gmail.com>
2017-06-06 18:13:23 +0300

    added files

---
+++ js/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        nextArrow: '<span class="slick-arrow right"><img src="img/next.svg"></span>',
+        prevArrow: '<span class="slick-arrow left"><img src="img/back.svg"></span>',
+        dots: true
+        // infinite: true,
+        // speed: 500,
+        // rtl: true
+    });
+
+
+
+
+
+});
